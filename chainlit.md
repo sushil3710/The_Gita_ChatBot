bd5bab4efa6b4bf48d45c0fe55cf115f39b51831
sushil3710 <2020csb1132@iitrpr.ac.in>
2023-10-01 01:36:38 +0530

    first commit

---
+++ chainlit.md
@@ -0,0 +1,5 @@
+# Welcome to The Gita ChatBot!
+
+Namaskar! I am here to help you explore the teachings of The Bhagwad Gita.
+Feel free to ask me any questions, seek guidance, or discuss its verses.
+You can start by asking a question or specifying a verse from The Bhagwad Gita.
---
+++ model.py
@@ -0,0 +1,107 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain import PromptTemplate
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import CTransformers
+from langchain.chains import RetrievalQA
+import chainlit as cl
+# import streamlit as st
+import os
+
+# st.set_page_config(
+#     page_title="The Bhagwat Gita Chatbot",
+#     page_icon="📖",  # Change to a suitable icon
+# )
+
+# Load and display an animated picture for the chatbot
+# chatbot_image_path = "user-avatar.gif"  # Update with the path to your animated image
+# st.sidebar.image(chatbot_image_path)
+
+
+
+DB_FAISS_PATH = 'vectorstore/db_faiss'
+
+custom_prompt_template = """The answers should be based on the trained data from The Bhagwat Gita about life lessons and all, If you do not know the answer then just
+say I don;t know the answer, Do not create answers from your own assumptions and be strict to teh answers based on The Bhagwat Gita.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
+
+def set_custom_prompt():
+    """
+    Prompt template for QA retrieval for each vectorstore
+    """
+    prompt = PromptTemplate(template=custom_prompt_template,
+                            input_variables=['context', 'question'])
+    return prompt
+
+#Retrieval QA Chain
+def retrieval_qa_chain(llm, prompt, db):
+    qa_chain = RetrievalQA.from_chain_type(llm=llm,
+                                       chain_type='stuff',
+                                       retriever=db.as_retriever(search_kwargs={'k': 2}),
+                                       return_source_documents=True,
+                                       chain_type_kwargs={'prompt': prompt}
+                                       )
+    return qa_chain
+
+#Loading the model
+def load_llm():
+    # Load the locally downloaded model here
+    llm = CTransformers(
+        model = "llama-2-7b-chat.ggmlv3.q8_0.bin",
+        model_type="llama",
+        max_new_tokens = 512,
+        temperature = 0.5
+    )
+    return llm
+
+#QA Model Function
+def qa_bot():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
+                                       model_kwargs={'device': 'cpu'})
+    db = FAISS.load_local(DB_FAISS_PATH, embeddings)
+    llm = load_llm()
+    qa_prompt = set_custom_prompt()
+    qa = retrieval_qa_chain(llm, qa_prompt, db)
+
+    return qa
+
+#output function
+def final_result(query):
+    qa_result = qa_bot()
+    response = qa_result({'query': query})
+    return response
+
+#chainlit code
+@cl.on_chat_start
+async def start():
+    chain = qa_bot()
+    msg = cl.Message(content="Starting the bot...")
+    await msg.send()
+    msg.content = "Namaskar!!, Welcome to the ChatBot. Let me answer your queries using The divine Bhagwad Gita.."
+    await msg.update()
+
+    cl.user_session.set("chain", chain)
+
+@cl.on_message
+async def main(message):
+    chain = cl.user_session.get("chain") 
+    cb = cl.AsyncLangchainCallbackHandler(
+        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
+    )
+    cb.answer_reached = True
+    res = await chain.acall(message, callbacks=[cb])
+    answer = res["result"]
+    sources = res["source_documents"]
+
+    if sources:
+        answer += f"\nSources:" + str(sources)
+    else:
+        answer += "\nNo sources found"
+
+    await cl.Message(content=answer).send()
