c68c0c60ee24c958043c0c5545f64212ba945ab5
sushil3710 <2020csb1132@iitrpr.ac.in>
2023-10-01 22:08:23 +0530

    bash script

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-chainlit run model.py -w
+#!/bin/bash
+
+# Build the Docker image
+docker build -t gita_chatbot .
+
+# Run the Docker container
+docker run -d -p 8080:80 gita_chatbot
+
+# Display container information
+echo "Docker container for Gita ChatBot is running."
+echo "You can access the chatbot at http://localhost:8080"
+
